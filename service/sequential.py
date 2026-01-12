a2b0966a140019b498dd457fb3a7bbd181ec1442
sarthaksrrri <sarthaksrrrivastava@gmail.com>
2026-01-12 12:43:28 +0530

    processed_r1_request

--- service/sequential.py
+++ service/sequential.py
@@ -1,7 +1,6 @@
 import requests
 import time
 import os
-# import json
 from dotenv import load_dotenv
 from utils.metrice import get_metrices
 
@@ -11,6 +10,9 @@ v1 = os.getenv("E1")
 v2 = os.getenv("E2")
 v3 = os.getenv("E3")
 
+price_list = []
+
+
 
 def run_sequential():
     start_time = time.time()
@@ -19,19 +21,51 @@ def run_sequential():
     r2 = requests.get(v2).json()
     r3 = requests.get(v3).json()
 
+
+    for i in r1.get("products"):
+        price_list.append(i["price"])
+    
+    max_price = price_list[0]
+
+    for i in range(0, len(price_list)-1):    
+        if max_price < price_list[i]:
+            max_price = price_list[i]
+    print(f"This is Max price :  {max_price}")
+
     end_memory = get_metrices()
     end_time = time.time()
     total_run_time = end_time - start_time 
+    r1_dict = []
+    for i in r1.get("products"):
+        r1_dict.append(i["rating"])
+    print(f"This is ratimg list :  {r1_dict}")
 
+    find_highest_rating = r1_dict[0]
+    for i in range(0,len(r1_dict)):
+        if find_highest_rating < r1_dict[i]:
+            find_highest_rating = r1_dict
+        
+
+    processed_Data = {
+        "status" : True,
+        "highest_price" : max_price,
+        "highest_rating" : find_highest_rating
+    }
 
     return{
 
-        "total_run_tim" : round(total_run_time , 3),
+        "total_run_time" : round(total_run_time , 3),
         "memory_usage" : round(end_memory - get_usage , 3),
         "r1" : r1,
         "r2" : r2,
         "r3" : r3
     }
+
+
+
+
+
+
 
 
 
